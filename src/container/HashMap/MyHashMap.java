299cd4dd0e0b6ff22c918759f03de87df8986774
chenzifeng1 <704734862@qq.com>
2020-04-12 14:55:35 +0800

    hashmap源码解析

--- src/container/HashMap/MyHashMap.java
+++ src/container/HashMap/MyHashMap.java
@@ -1,18 +1,65 @@
 package container.HashMap;
 
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
 import java.io.Serializable;
 import java.util.*;
 
 
 public class MyHashMap<K, V> extends AbstractMap<K, V>
         implements Map<K, V>, Cloneable, Serializable {
+
+    //序列号
+    private static final long serialVersionUID = 362498820763181265L;
+    //默认初试容量
+    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4; // aka 16
+    //最大容量
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+    //默认填充因子
+    static final float DEFAULT_LOAD_FACTOR = 0.75f;
+    //当桶(bucket)节点数大于该值就会转换成红黑树
+    static final int TREEIFY_THRESHOLD = 8;
+    //当桶(bucket)节点数小于该值就会转换成链表
+    static final int UNTREEIFY_THRESHOLD = 6;
+    //桶中结构转换成红黑树对应的table的最小容量
+    static final int MIN_TREEIFY_CAPACITY = 64;
+    //存放具体node的数组，第一次使用时初始化，用来存放数据，总是2的幂次倍
+    transient Node<K, V>[] table;
+    //存放具体元素的集合
+    transient Set<Map.Entry<K, V>> entrySet;
+
+    transient int size;
+    transient int modCount;
+    //临界值：当容量*装填因子超过该值就会进行扩容
+    int threshold;
+    //hash表的装填因子
+    float loadFactor;
+
+    /*-------------------------------constructor------------------------------------*/
+
+    public MyHashMap() {
+        this.loadFactor = DEFAULT_LOAD_FACTOR;
+    }
+
+    public MyHashMap(int initialCapacity){
+        this(initialCapacity,DEFAULT_LOAD_FACTOR);
+    }
+
     /**
-     * 默认初试化容量为16
+     * 构造方法：判断初始化容量是否在合法范围，以及获取
+     * @param initialCapacity 初始化容量
+     * @param loadFactor 装填因子
      */
-    static final int DEFAULT_INITIAL_CAPACITY = 1 << 4;     //值为16
 
-    static final int MAX_CAPACITY = 1 << 30;
-
+    public MyHashMap(int initialCapacity, float loadFactor) {
+        if (initialCapacity < 0)
+            throw new IllegalArgumentException("Illegal initial capacity :" + initialCapacity);
+        if (initialCapacity > MAXIMUM_CAPACITY)
+            initialCapacity = MAXIMUM_CAPACITY;
+        if (loadFactor < 0 || Float.isNaN(loadFactor))
+            throw new IllegalArgumentException("Illegal load Factor :" + loadFactor);
+        this.loadFactor = loadFactor;
+        this.threshold = tableSizeFor(initialCapacity);
+    }
 
     /**
      * 内部节点类Node,JDK1.8之后，会将在解决冲突时，将长度超过8的链表转化为红黑树
@@ -35,6 +82,7 @@ public class MyHashMap<K, V> extends AbstractMap<K, V>
 
         /**
          * final方法不能被子类重写
+         *
          * @return
          */
         public final K getKey() {
@@ -54,10 +102,11 @@ public class MyHashMap<K, V> extends AbstractMap<K, V>
         /**
          * 重写Node的hashCode: 其值为 key的hashCode与value的hashCode做异或运算
          * 异或位运算：1^1 = 0 ;  1^0 = 1
+         *
          * @return
          */
-        public final int hashCode(){
-            return Objects.hashCode(key)^Objects.hashCode(value);
+        public final int hashCode() {
+            return Objects.hashCode(key) ^ Objects.hashCode(value);
         }
 
         public final V setValue(V newValue) {
@@ -74,19 +123,31 @@ public class MyHashMap<K, V> extends AbstractMap<K, V>
          * 如果参数节点为Map.Entry的一个实例，则分别判断两节点的key,value是否对应相等。
          * 注：判断key,value时不能用 == 来判断，因为key,value可能为引用类型。
          * 所以，应该使用Objects的equals(Object,Object)方法判断
+         *
          * @param o
          * @return
          */
-        public final boolean equals(Object o){
-            if(this == o)
+        public final boolean equals(Object o) {
+            if (this == o)
                 return true;
-            if (o instanceof Map.Entry){
-                Map.Entry<?,?> e = (Entry<?, ?>) o;
-                if(Objects.equals(e.getKey(),key)&&Objects.equals(e.getValue(),value))
+            if (o instanceof Map.Entry) {
+                Map.Entry<?, ?> e = (Entry<?, ?>) o;
+                if (Objects.equals(e.getKey(), key) && Objects.equals(e.getValue(), value))
                     return true;
             }
             return false;
         }
+
+
+    }
+    /*--------------------protected way (保护类方法区）----------------------*/
+
+    /**
+     *
+     * @param map
+     * @param exivt 如果map已经被初始化了，则为false，否则为true
+     */
+    final void putMapEntries(Map<? extends K,? extends V> map,boolean exivt){
 
 
     }
@@ -108,4 +169,57 @@ public class MyHashMap<K, V> extends AbstractMap<K, V>
     public Set<Entry<K, V>> entrySet() {
         return null;
     }
+
+    /**
+     *
+     * @param cap
+     * @return
+     */
+    static final int tableSizeFor(int cap) {
+        int n = -1 >>> Integer.numberOfLeadingZeros(cap - 10);
+        return (n < 0) ? 1 : (n > MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+
+    /*--------------------------------TreeNode------------------------------------------------------------*/
+
+
+    /**
+     * 内部红黑树节点类
+     * 红黑树：节点要么是黑色，要么是红色
+     * 1.根节点和叶节点为黑色
+     * 2.红节点的两个子节点为黑色
+     * 3.从任一节点到叶节点的路径包含相同数量的黑色节点-如果某节点A的子节点为黑色，则节点A有两个黑色子节点。
+     * 不然无法满足从该节点到可达的叶节点路径上黑色节点相同。
+     *
+     */
+    /*
+    static final class TreeNode extends LinkedHashMap.Entry<K,V>{
+        TreeNode<K,V> parents; //父节点
+        TreeNode<K,V> left;//左孩子
+        TreeNode<K,V> right;//右孩子
+        TreeNode<K,V> pre;//删除节点时应断开链接
+        boolean red;//是否为红节点
+
+        TreeNode(int hash,K key,V value,Node<K,V> next) {
+            super(hash,key,value,next);
+        }
+
+        **
+         * 得到红黑树的根节点，通过迭代不断向上遍历，直到找到parents为空的节点，该节点即为根节点
+         * @return
+         *
+        final TreeNode root(){
+            for(TreeNode<K,V> r = this,p;;){
+               if((p = r.parents)==null)
+                   return r;
+               r = p;
+            }
+        }
+
+
+
+    }
+    */
+
+
 }
