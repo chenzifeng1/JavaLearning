3eb3a22045c5c2ff1bc0ae171a3a422c6ea53361
chenzifeng1 <704734862@qq.com>
2024-02-29 23:13:28 +0800

    Create Constants.java

---
+++ src/main/java/org/algorithm/test/memory/Constants.java
@@ -0,0 +1,17 @@
+package org.algorithm.test.memory;
+
+/**
+ * @author chenzifeng
+ * @version 1.0
+ * @description
+ * @date 2024/2/29 11:42 AM
+ */
+public class Constants {
+
+    public static final int DEFAULT_PAGE_SIZE = 4096;
+
+    /**
+     * 内存池最大容量
+     */
+    public final static int MAX_CAPACITY = 1024;
+}
